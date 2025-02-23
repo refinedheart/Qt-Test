62766f251697a1b7b0921510f1e6649016fe1d5b
refinedheart <50239539+refinedheart@users.noreply.github.com>
2025-02-23 10:02:10 +0800

    just a test change

--- just-test/widget.cpp
+++ just-test/widget.cpp
@@ -1,11 +1,12 @@
 #include "widget.h"
 #include "ui_widget.h"
-
+#include <QPushButton>
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    ui->btn1->setText("wdf???");
 }
 
 Widget::~Widget()
